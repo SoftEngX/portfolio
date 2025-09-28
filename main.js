82d7a43b01a732d8f53509998476bae4359c80a4
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2025-09-28 21:29:03 +0600

    fix style theme-toggle

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-
 const toggle = document.getElementById('themeToggle');
 const body = document.body;
 
+const sunIcon = document.querySelector('.sun-icond');
+const moonIcon = document.querySelector('.moon-icon');
+
 toggle.addEventListener('click', () => {
-  body.classList.toggle('dark');
   body.classList.toggle('light');
+
+  if (body.classList.contains('light')) {
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'inline';
+  } else {
+    sunIcon.style.display = 'inline';
+    moonIcon.style.display = 'none';
+  }
 });
 
+// при загрузке страницы показываем только солнце (тёмная тема по умолчанию)
+sunIcon.style.display = 'inline';
+moonIcon.style.display = 'none';
